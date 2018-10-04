ec8fadffd9c4f114a9c1f07d29471f0b02a7d97f
갓거 <taylored@naver.com>
2018-10-04 21:06:31 +0900

    no message

--- C++_Algorithm/C++Algorithm/1단계/문자열 다루기 기본.cpp
+++ C++_Algorithm/C++Algorithm/1단계/문자열 다루기 기본.cpp
@@ -1,28 +1,28 @@
+////
+////  문자열 다루기 기본.cpp
+////  C++Algorithm
+////
+////  Created by 갓거 on 2018. 10. 3..
+////  Copyright © 2018년 갓거. All rights reserved.
+////
 //
-//  문자열 다루기 기본.cpp
-//  C++Algorithm
+//#include <string>
+//#include <iostream>
+//#include <regex>
 //
-//  Created by 갓거 on 2018. 10. 3..
-//  Copyright © 2018년 갓거. All rights reserved.
+//using namespace std;
 //
-
-#include <string>
-#include <iostream>
-#include <regex>
-
-using namespace std;
-
-bool solution(string s) {
-    bool answer = true;
-    regex number("^[0-9]*");
-    if (s.length() == 4 || s.length() == 6) {
-        answer = regex_match(s, number);
-    } else {
-        answer = false;
-    }
-    return answer;
-}
-
-int main(){
-    cout << solution("sss") << endl;
-}
+//bool solution(string s) {
+//    bool answer = true;
+//    regex number("^[0-9]*");
+//    if (s.length() == 4 || s.length() == 6) {
+//        answer = regex_match(s, number);
+//    } else {
+//        answer = false;
+//    }
+//    return answer;
+//}
+//
+//int main(){
+//    cout << solution("sss") << endl;
+//}
